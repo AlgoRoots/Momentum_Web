70024f1ac40591410ad8a2a4a9cd84643382f6ad
AlgoRoots <algoroot524@gmail.com>
2022-02-21 16:54:49 +0900

    Edit wather.js

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ function onGeoOk(position) {
       const weather = document.querySelector("#weather span:first-child");
       const city = document.querySelector("#weather span:last-child");
       city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
     });
 }
 function onGeoError() {
